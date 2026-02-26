24cd25438035da294ead4d0cf0ba7677a358b285
Luis Sanz <luis.sanz@colba.es>
2026-02-26 17:45:17 +0100

    Add services each request case

--- ProducerConsumer/Producer/Repository/IMessageRepository.cs
+++ ProducerConsumer/Producer/Repository/IMessageRepository.cs
@@ -5,4 +5,7 @@ namespace Producer.Repository;
 public interface IMessageRepository
 {
     Task SaveReceivedMessageAsync(ReceivedMessage message);
+    Task SaveProcessedMessageAsync(ProcessedMessage message);
+    Task SaveProcessingMessageAsync(ProcessingMessage message);
+    Task SaveErrorMessageAsync(ErrorMessage message);
 }
