adb13ea9f87cd987f9aedc7b29cc9ee728e9d9b5
Fran R. <fran.ruiz@colba.es>
2026-02-26 13:01:14 +0100

    CleanUp

--- ProducerConsumer/Consumer/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Consumer.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- ProducerConsumer/Producer/Program.cs
+++ ProducerConsumer/Producer/Program.cs
@@ -2,8 +2,9 @@ using Raven.Client.Documents;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IDocumentStore>(_ =>
 {
     var store = new DocumentStore
@@ -11,30 +12,18 @@ builder.Services.AddSingleton<IDocumentStore>(_ =>
         Urls = ["http://localhost:8080"],
         Database = "Hackaton"
     };
-
     store.Initialize();
     return store;
 });
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapStaticAssets();
-
-app.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+app.MapControllers();
 app.Run();
